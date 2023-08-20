31ea7a40658b4e47144c73090c0e1d4b4565560a
Frank Williames <dev.frankw@gmail.com>
2023-08-19 23:24:39 -0400

    adicionando a função de barra de progresso para os atributos

--- desafio-1/desafio.lua
+++ desafio-1/desafio.lua
@@ -2,13 +2,45 @@
 os.execute("chcp 65001") -- muda a codificação do terminal para UTF-8
 os.execute("cls")
 
+
+--Criatura
 local monsterName = "CREEPER"
 local description = "Um monstro furtivo com temperamento explosivo"
 local emoji ="💥"
 
+--Atributos
+local attackAttribute = 10
+local defenseAttribute = 1
+local vitalityAttribute = 5
+local speedAttribute = 7
+local inteligenceAttribute = 2
+
+local function getProgressBar(attribute)
+    local fullChar = "⬜"
+    local emptyChar = "⬛"
+    local result = ""
+
+    for i = 1, 10, 1 do
+        if i <= attribute then
+            result = result .. fullChar
+        else
+            result = result .. emptyChar
+        end
+        
+    end
+    return result
+end
+--Cartao
 print("=================================================")
 print("| " .. monsterName) -- ".." é o operador de concatenção
 print("| " .. description)
 print("| ")
 print("| Emoji Favorito: " .. emoji)
+print("| Atributos")
+print("|    Ataque:       " .. getProgressBar(attackAttribute))
+print("|    Defesa:       " .. getProgressBar(defenseAttribute))
+print("|    Vida:         " .. getProgressBar(vitalityAttribute))
+print("|    Velocidade:   " .. getProgressBar(speedAttribute))
+print("|    Inteligencia: " .. getProgressBar(inteligenceAttribute))
+print("| ")
 print("=================================================")
