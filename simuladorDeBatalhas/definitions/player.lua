97615a7d825a2ad49c97206a7523414f37355113
Frank Williames <dev.frankw@gmail.com>
2023-08-21 19:32:42 -0400

    criando as definicoes

---
+++ simuladorDeBatalhas/definitions/player.lua
@@ -0,0 +1,11 @@
+local player = {}
+    player.name = "Violet"
+    player.description = "Uma guerreira espadachim"
+    player.maxHealth = 10
+    player.maxHealth = 10
+    player.attack = 4
+    player.defense = 2
+    player.speed = 3
+
+
+return player
