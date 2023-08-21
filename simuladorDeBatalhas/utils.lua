9b12c94d91695a971fddc45ae3278fc44b9083a4
Frank Williames <dev.frankw@gmail.com>
2023-08-21 19:08:33 -0400

    criando a funcao reader, utilizando as funcoes card limit no modulo utils

--- simuladorDeBatalhas/utils.lua
+++ simuladorDeBatalhas/utils.lua
@@ -8,4 +8,42 @@ function utils.enableUtf8()
     os.execute("cls") 
 end
 
+--[[
+    Displays a line composed of equal characters, creating a "limit card".
+    This card is used for visual separation or delimitation in text outputs.
+
+    Usage:
+    utils.cardLimite()
+]]
+function utils.cardLimite()
+    local limit = "="
+    local result = ""
+    for i = 1, 100, 1 do
+        result = result .. limit
+    end
+    print(result)
+end
+
+--[[
+    Displays a stylized header with a title and a boundary line, creating a visual presentation.
+
+    Usage:
+    utils.header()
+]]
+function utils.header()
+    print(utils.cardLimite())
+    print([[
+              /\                                                                       /\
+    _         )( ______________________                         ______________________ )(         _
+   (_)///////(**)______________________> Simulador de Batalhas <______________________(**)\\\\\\\(_)
+              )(                                                                       )(
+              \/                                                                       \/
+                  
+    ]])
+    print(utils.cardLimite())
+    print([[
+                                Voce Empunha sua espada e se prepara para lutar.
+                                        ⚔ É hora da batalha!!! ⚔
+    ]]) 
+end
 return utils
