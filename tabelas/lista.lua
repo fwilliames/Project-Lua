05ec05eeee5d92a776915e5078f8701650b37ad9
Frank Williames <dev.frankw@gmail.com>
2023-08-21 02:09:49 -0400

    adicionando informacoes sobre tabelas

--- tabelas/lista.lua
+++ tabelas/lista.lua
@@ -1,5 +1,5 @@
 local names = {"creeper", "zombie", "skeleton","phatom"} --inicializando uma tabela
-local lengthNames = #names -- "#nomeTabela" retorna o tamanho da tabela
+local lengthNames = #names -- "#nomeTabela" retorna o tamanho da tabela enquanto
 
 for i = 1, lengthNames, 1 do
     print(names[i])
