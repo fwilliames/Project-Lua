51b5ed91bc1f431c505815f518125e432de7c202
Frank Williames <dev.frankw@gmail.com>
2023-08-21 17:53:38 -0400

    finalizando o modulo 2

--- modulos/main.lua
+++ modulos/main.lua
@@ -1,8 +1,10 @@
 local utils = require("utils")
+local sheep = require("creatures/sheep")
 utils.enableUtf8()
 
 print("Hello from main.lua")
 utils.helloFromUtils()
+sheep.talk()
 
 for i = 0, 10, 1 do
     print(
