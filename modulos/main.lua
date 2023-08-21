ea512bf463255f7498e7fa1e25b02a201bba4a66
Frank Williames <dev.frankw@gmail.com>
2023-08-21 16:46:56 -0400

    adicionando documentacao ao modulo utils.lua

--- modulos/main.lua
+++ modulos/main.lua
@@ -1,4 +1,11 @@
 local utils = require("utils")
+utils.enableUtf8()
 
 print("Hello from main.lua")
 utils.helloFromUtils()
+
+for i = 0, 10, 1 do
+    print(
+        utils.getProgressBar(i)
+    )
+end
