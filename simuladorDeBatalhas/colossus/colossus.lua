f152676b63891cd8cdc295b7cba9031535bffa52
Frank Williames <dev.frankw@gmail.com>
2023-08-22 03:13:51 -0400

    nerf na vida e no ataque do colossus

--- simuladorDeBatalhas/colossus/colossus.lua
+++ simuladorDeBatalhas/colossus/colossus.lua
@@ -1,9 +1,9 @@
 local colossus = {}
     colossus.name = "Prismarine Colossus"
     colossus.description = "Enorme estatua de Prismarine que mede mais de 20m de altura"
-    colossus.maxHealth = 50
-    colossus.health = 50
-    colossus.attack = 5
+    colossus.maxHealth = 20
+    colossus.health = 20
+    colossus.attack = 3
     colossus.defense = 6
     colossus.speed = 2
 
