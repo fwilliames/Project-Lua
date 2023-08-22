970a36b70e5ad9487e7ff935bab8390bb20a5aab
Frank Williames <dev.frankw@gmail.com>
2023-08-22 01:48:38 -0400

    adicionando turno do player

--- simuladorDeBatalhas/main.lua
+++ simuladorDeBatalhas/main.lua
@@ -1,16 +1,37 @@
 local utils = require("utils")
-local player = require("simuladorDeBatalhas.player.player")
-local colossus = require("simuladorDeBatalhas.colossus.colossus")
+local player = require("player.player")
+local playerActions = require("player.actions")
+local colossus = require("colossus.colossus")
 
 utils.enableUtf8()
 utils.header()
 
 local boss = colossus
 utils.printCard(boss)
+playerActions.build()
 
 
 while true do
     --Player Turn
+    print("O que voce deseja fazer?")
+    
+    local validPlayerActions = playerActions.getValidActions(player,boss) --acrescentar ao utils uma funcao displayActions
+    for i, action in pairs(validPlayerActions) do
+        print(
+            string.format("%d. %s",i, action.description)
+        )
+    end
+
+    local chosenId = utils.ask()
+    local chosenAction = validPlayerActions[chosenId]
+    local isActionValid = chosenAction ~= nil
+
+    if isActionValid then
+        chosenAction.execute(player,boss)
+    else
+        print("Sua acao eh invalida")
+    end
+
     if boss.health <= 0 then
         break
     end
