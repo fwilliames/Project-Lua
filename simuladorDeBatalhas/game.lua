9832319f2227b48fa401f91025fece7100ad5b57
Frank Williames <dev.frankw@gmail.com>
2023-08-22 03:57:48 -0400

    adicionando documentacao da funcao displayActions

--- simuladorDeBatalhas/game.lua
+++ simuladorDeBatalhas/game.lua
@@ -1,4 +1,19 @@
 local game = {}
+
+    --[[
+        Displays valid actions available to the player along with their descriptions.
+
+        Parameters:
+        - playerData: A table containing player data.
+        - playerActions: A table containing player actions.
+        - creatureData: A table containing creature data.
+
+        Returns:
+        A table containing valid actions available to the player.
+
+        Usage:
+        local validActions = game.displayActions(playerData, playerActions, creatureData)
+    ]]
     function game.displayActions(playerData,playerActions, creatureData)
         local validheroActions = playerActions.getValidActions(playerData,creatureData) --acrescentar ao utils uma funcao displayActions
         for i, action in pairs(validheroActions) do
@@ -8,4 +23,7 @@ local game = {}
         end
         return validheroActions
     end
+
+    
+
 return game
