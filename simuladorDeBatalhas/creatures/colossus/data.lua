209deefc012322686dc952a6f66211894811b38f
Frank Williames <dev.frankw@gmail.com>
2023-08-22 16:30:41 -0400

    ajuste de nomemclatura dos dados do jogo

---
+++ simuladorDeBatalhas/creatures/colossus/data.lua
@@ -0,0 +1,10 @@
+local data = {}
+    data.name = "Prismarine data"
+    data.description = "Enorme estatua de Prismarine que mede mais de 20m de altura"
+    data.maxHealth = 20
+    data.health = 20
+    data.attack = 3
+    data.defense = 6
+    data.speed = 2
+
+return data
---
+++ simuladorDeBatalhas/hero/violet/data.lua
@@ -0,0 +1,10 @@
+local data = {}
+    data.name = "data Van Wision"
+    data.description = "Uma guerreira espadachim"
+    data.maxHealth = 10
+    data.health = 10
+    data.attack = 4
+    data.defense = 2
+    data.speed = 3
+    data.potion = 3
+return data
