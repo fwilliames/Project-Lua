2cd9b0cd479262302426f8f3991da173b38a7c2c
Frank Williames <dev.frankw@gmail.com>
2023-08-21 23:30:58 -0400

    criando a pasta com os arquivos do colossus

---
+++ simuladorDeBatalhas/colossus/colossus.lua
@@ -0,0 +1,10 @@
+local colossus = {}
+    colossus.name = "Prismarine Colossus"
+    colossus.description = "Enorme estatua de Prismarine que mede mais de 20m de altura"
+    colossus.maxHealth = 50
+    colossus.health = 50
+    colossus.attack = 5
+    colossus.defense = 6
+    colossus.speed = 0
+
+return colossus
