cbecfad4712430a61758cdbc8dc9a28cd0fc56d4
Frank Williames <dev.frankw@gmail.com>
2023-08-21 20:40:26 -0400

    adicionando o loop do game

--- simuladorDeBatalhas/main.lua
+++ simuladorDeBatalhas/main.lua
@@ -7,3 +7,16 @@ utils.header()
 
 local boss = colossus
 utils.printCard(boss)
+
+
+while true do
+    --Player Turn
+    if boss.health <= 0 then
+        break
+    end
+
+    --Boss Turn
+    if player.health <= 0 then
+        break
+    end
+end
